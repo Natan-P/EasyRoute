26cc493cd779210485e801047c393a008ffa91f2
Natan <npolenec@gmail.com>
2022-04-10 10:24:45 +0200

    added full custom direction support

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def generatePath(paths):
     pass
 
 def parsePath(i, s):
+    c = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+    t = "".join([baseDirs[directions.index(x)] for x in i if not (x in c)])
+    n = "".join([x for x in i if x in c])
+
     p = {"n": -boardSize, "nw": -boardSize+1,
          "w": -1, "sw": boardSize-1,
          "s": boardSize, "se": boardSize+1,
          "e": 1, "ne": -boardSize-1}
-    c = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-    t = "".join([x for x in i if not(x in c)])
-    n = "".join([x for x in i if x in c])
     if n == "":
         n = 1
     else:
@@ -23,9 +24,10 @@ def parsePath(i, s):
 boardSize = 6
 boardIndicators = [["1", "2", "3", "4", "5", "6"],
                    ["A", "B", "C", "D", "E", "F"]]  # how the board is indicated at the sides (1st E-W, 2nd N-S)
-directions = ["N", "W", "S", "E"]  # cardinal direction names of the board (direction definition going ↑→↓←)
+directions = ["S", "V", "J", "Z"]  # cardinal direction names of the board (direction definition going ↑→↓←)
+baseDirs = ["n", "w", "s", "e"]  # cardinal directions that are used in the code
 pathNums = [20, 15, 10, 5]
 if __name__ == "__main__":
-    path = "sw"
-    print(parsePath(path, 5))
-#newBoard(boardSize)
+    path = "2SV"
+    print(parsePath(path, 13))
+# newBoard(boardSize)
